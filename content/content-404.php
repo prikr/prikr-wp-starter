a3823ed315232d379cb583a81588e2cba8c01128
Jasper de Eindprikr <jasper@prikr.io>
2021-09-21 16:47:20 +0200

    progress

--- content/content-404.php
+++ content/content-404.php
@@ -11,8 +11,18 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 ?>
 
-<section name="404" class="s-100 404">
+<section name="404" class="s-100 404 d-flex flex-column align-items-center justify-content-center">
 
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>404</h1>
+        <span class="d-block fs-3">Sorry! Deze pagina bestaat niet (meer).</span>
+        <a href="/" class="btn btn-secondary margin-top-48">Naar homepagina</a>
+      </div>
+    </div>
+  </div>
 
+  <span class="bg-404">404</span>
 
 </section>
